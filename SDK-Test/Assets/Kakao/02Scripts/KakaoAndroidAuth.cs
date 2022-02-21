adba31691328710f60a78ccc7151405cfab55e76
LeeChangHo-James <chho1365@gmail.com>
2022-02-21 17:33:19 +0900

    2022-02-21

---
+++ SDK-Test/Assets/Kakao/02Scripts/KakaoAndroidAuth.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KakaoAndroidAuth : MonoBehaviour
+{
+    [SerializeField]
+    private Text infoText;
+
+    public void InformationClear()
+    {
+        infoText.text = "";
+    }
+
+    public void KakaoSignIn()
+    {
+        infoText.text += "KakaoSignIn\n";
+        AndroidJavaObject plugin = new AndroidJavaObject("com.unity3d.player.KakaoAuth");
+        plugin.Call("KakaoSignIn");
+    }
+
+    public void KakaoSignOut()
+    {
+        infoText.text += "KakaoSignOut\n";
+        AndroidJavaObject plugin = new AndroidJavaObject("com.unity3d.player.KakaoAuth");
+        plugin.Call("KakaoSignOut");
+    }
+
+    /* 
+     * [ 사용 주의 ]
+     * 카카오 계정연결을 해제하기 위해서는 로그인이 되어있어야한다.
+     * 로그인 후 -> 계정연결 해제하도록 하자
+     */
+    public void KakaoUnlink()
+    {
+        infoText.text += "KakaoUnlink\n";
+        AndroidJavaObject plugin = new AndroidJavaObject("com.unity3d.player.KakaoAuth");
+        plugin.Call("KakaoUnlink");
+    }
+
+    private void KakaoToken(string call)
+    {
+        infoText.text += $"token : {call}\n";
+    }
+
+    private void KakaoID(string call)
+    {
+        infoText.text += $"id : {call}\n";
+    }
+
+    private void KakaoName(string call)
+    {
+        infoText.text += $"name : {call}\n";
+    }
+
+    private void KakaoEmail(string call)
+    {
+        infoText.text += $"email : {call}\n"; 
+    }
+
+    private void KakaoProfileImage(string call)
+    {
+        infoText.text += $"profileImage : {call}\n";
+    }
+
+    private void KakaoPhoneNumber(string call)
+    {
+        infoText.text += $"phoneNumber : {call}\n";
+    }
+
+    private void KakaoEvent(string call)
+    {
+        switch(call)
+        {
+            case "signout":
+                infoText.text += $"로그아웃 성공\n";
+                break;
+            case "unlink":
+                infoText.text += $"계정연결해제 성공\n";
+                break;
+            default:
+                infoText.text += $"알수없는 이벤트입니다.\n";
+                break;
+        }
+    }
+
+    private void KakaoError(string call)
+    {
+        switch(call)
+        {
+            case "signin":
+                infoText.text += $"로그인 에러 발생\n";
+                break;
+            case "signout":
+                infoText.text += $"로그아웃 에러 발생\n";
+                break;
+            case "unlink":
+                infoText.text += $"계정연결 해제 에러 발생\n";
+                break;
+            case "userinfo":
+                infoText.text += $"사용자 정보 불러오기 에러 발생\n";
+                break;
+        }
+    }
+}
---
+++ SDK-Test/Assets/Plugins/Android/Kakao/KakaoAuth.kt
@@ -0,0 +1,68 @@
+package com.unity3d.player
+
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.user.UserApiClient
+import com.unity3d.player.UnityPlayer
+
+class KakaoAuth {
+
+    fun KakaoSignIn() {
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null) {
+                UnityPlayer.UnitySendMessage("GameManager", "KakaoError", "signin")
+            } else if (token != null) {
+                UnityPlayer.UnitySendMessage("GameManager", "KakaoToken", "${token.accessToken}")
+                KakaoUserInfo()
+            }
+        }
+
+        // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(UnityPlayer.currentActivity)) {
+            UserApiClient.instance.loginWithKakaoTalk(
+                UnityPlayer.currentActivity,
+                callback = callback
+            )
+        } else {
+            UserApiClient.instance.loginWithKakaoAccount(
+                UnityPlayer.currentActivity,
+                callback = callback
+            )
+        }
+    }
+
+    fun KakaoSignOut() {
+        UserApiClient.instance.logout { error ->
+            if (error != null) {
+                UnityPlayer.UnitySendMessage("GameManager", "KakaoError", "signout")
+            } else {
+                UnityPlayer.UnitySendMessage("GameManager", "KakaoEvent", "signout")
+            }
+        }
+    }
+
+    fun KakaoUnlink() {
+        UserApiClient.instance.unlink { error ->
+            if (error != null) {
+               UnityPlayer.UnitySendMessage("GameManager", "KakaoError", "unlink") 
+            } else {
+               UnityPlayer.UnitySendMessage("GameManager", "KakaoEvent", "unlink") 
+            }
+        }
+    }
+
+
+    fun KakaoUserInfo(){
+        UserApiClient.instance.me { user, error ->
+            if (error != null) {
+                UnityPlayer.UnitySendMessage("GameManager", "KakaoError", "userinfo")
+            }
+            else if (user != null) {				
+                UnityPlayer.UnitySendMessage("GameManager", "KakaoID", "${user.id}")
+                UnityPlayer.UnitySendMessage("GameManager", "KakaoName", "${user.kakaoAccount?.profile?.nickname}")
+				UnityPlayer.UnitySendMessage("GameManager", "KakaoEmail", "${user.kakaoAccount?.email}")
+                UnityPlayer.UnitySendMessage("GameManager", "KakaoProfileImage", "${user.kakaoAccount?.profile?.thumbnailImageUrl}")
+				UnityPlayer.UnitySendMessage("GameManager", "KakaoPhoneNumber", "${user.kakaoAccount?.phoneNumber}")
+            }
+        }
+    }
+}
